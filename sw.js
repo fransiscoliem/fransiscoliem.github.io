34fc5444103fd8f34d088a80d3921e758d761a13
Fransisco <fransiscoliemena@gmail.com>
2019-06-04 17:14:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -43,46 +43,46 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
-  var url = 'https://httpbin.org/get';
+// self.addEventListener('fetch', function (event) {
+//   var url = 'https://httpbin.org/get';
 
-  if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              cache.put(event.request, res.clone());
-              return res;
-            });
-        })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function (response) {
-          if (response) {
-            return response;
-          } else {
-            return fetch(event.request)
-              .then(function (res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function (cache) {
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
-              })
-              .catch(function (err) {
-                return caches.open(CACHE_STATIC_NAME)
-                  .then(function (cache) {
-                    return cache.match('/offline.html');
-                  });
-              });
-          }
-        })
-    );
-  }
-});
+//   if (event.request.url.indexOf(url) > -1) {
+//     event.respondWith(
+//       caches.open(CACHE_DYNAMIC_NAME)
+//         .then(function (cache) {
+//           return fetch(event.request)
+//             .then(function (res) {
+//               cache.put(event.request, res.clone());
+//               return res;
+//             });
+//         })
+//     );
+//   } else {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then(function (response) {
+//           if (response) {
+//             return response;
+//           } else {
+//             return fetch(event.request)
+//               .then(function (res) {
+//                 return caches.open(CACHE_DYNAMIC_NAME)
+//                   .then(function (cache) {
+//                     cache.put(event.request.url, res.clone());
+//                     return res;
+//                   })
+//               })
+//               .catch(function (err) {
+//                 return caches.open(CACHE_STATIC_NAME)
+//                   .then(function (cache) {
+//                     return cache.match('/offline.html');
+//                   });
+//               });
+//           }
+//         })
+//     );
+//   }
+// });
 
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
@@ -110,21 +110,21 @@ self.addEventListener('fetch', function (event) {
 //   );
 // });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     fetch(event.request)
-//       .then(function(res) {
-//         return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//       })
-//       .catch(function(err) {
-//         return caches.match(event.request);
-//       })
-//   );
-// });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(res) {
+        return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+      })
+      .catch(function(err) {
+        return caches.match(event.request);
+      })
+  );
+});
 
 // Cache-only
 // self.addEventListener('fetch', function (event) {
